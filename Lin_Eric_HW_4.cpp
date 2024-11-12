485f04e57354edc0c5959e747649286e94ae2d02
ericl343 <ericl343@nycstudents.net>
2024-11-12 15:35:51 -0500

    draft

---
+++ Lin_Eric_HW_4.cpp
@@ -0,0 +1,19 @@
+//Eric Lin
+//Homework #4
+//November 12th
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int myArray(int, int, int, int, int, int, int, int)
+
+int main () {
+    int c1,c2,c3,c4,c5,c6,c7,c8;
+
+    cout << "Enter 8 intergers seperated by space:" << endl;
+    cin >> c1 >> c2 >> c3 >> c4 >> c5 >> c6 >> c7 >> c8; 
+    cout << myArray(c1,c2,c3,c4,c5,c6,c7,c8);
+
+return 0;
+}
